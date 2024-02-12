90d5f5e9391c2dbf0f8554f2436747870822c3ad
betjones <betjones@wccnet.edu>
2024-02-11 20:47:10 -0500

    updates2

--- assignments/assignment2/exercise3.php
+++ assignments/assignment2/exercise3.php
@@ -1,42 +1,29 @@
+<?php
+$row = 15;
+$col = 5;
+$table = "<table class='table table-bordered'>";
+for ($i = 1; $i <= $row; $i++) {
+    $table .= "<tr>";
+    for ($j = 1; $j <= $col; $j++) {
+        $table .= "<td>Row $i, Col $j</td>";
+    }
+    $table .= "</tr>";
+}
+$table .= "</table>";
+?>
+
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Nested List</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
-  <body>
-  <div class = "container">
-  <table class="table table-bordered">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-</div>
- </body>
+</head>
+<body>
+    <div class="container">
+
+        <?php echo $table; ?>
+    </div>
+</body>
 </html>
